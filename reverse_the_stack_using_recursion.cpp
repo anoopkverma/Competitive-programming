0986903ceb29fb9889ca59cecafe2b7400c7a73b
Anoop Verma <anoopvermastp@gmail.com>
2016-12-03 21:07:03 +0530

    Create reverse_the_stack_using_recursion.cpp

---
+++ reverse_the_stack_using_recursion.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+stack<int> st;
+void insert_at_bottom(int curr)
+{
+    if(st.empty())   //stack is empty means bottom of the stack,insert the current element
+        st.push(curr);
+    else
+    {
+        int el=st.top();
+        st.pop();
+        insert_at_bottom(curr);
+        st.push(el);
+    }
+}
+void reverse()
+{
+    if(st.empty())
+        return;
+    int d=st.top();
+    st.pop();
+    reverse();
+    insert_at_bottom(d); //Inserting an element at bottom of the stack.
+}
+int main()
+{
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    reverse();
+    while(!st.empty())
+    {
+        cout<<st.top()<<endl;
+        st.pop();
+    }
+    return 0;
+}
